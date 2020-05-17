ac000/mtd-cli

commit 109a0c93626ef5334458dc816d2fad6a69322d39

mtd-cli: Do some refactoring
Put in some infrastructure to aid in adding support for more endpoints
in the future.

It's now mainly just adjusting/creating some #defines, creating a
endpoint_help structure array, adding an entry to the new api_ep_map[]
structure array and a creating a function for calling the libmtdac API
functions.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-cli.c
@@ -23,6 +23,15 @@
 #define MTD_CLI_SA	MTD_CLI " sa "
 #define MTA_CLI_SAAC	MTD_CLI " saac "
 
+static const struct api_ep {
+	const char *api;
+	int (*api_func)(int argc, char *argv[], char **buf);
+} api_ep_map[] = {
+	{ "sa", &do_sa },
+	{ "saac", &do_saac },
+	{ NULL, NULL }
+};
+
 struct endpoint_help {
 	const char *ep;
 	const char *use;
@@ -51,6 +60,7 @@ static const struct endpoint_help sa_endpoint_help[] = {
 	{ "get-end-of-period-statement",
 	   MTD_CLI_SA "get-end-of-period-statement selfEmploymentId start|- end|-",
 	   3 },
+	{ NULL, NULL, 0 }
 };
 
 static const struct endpoint_help saac_endpoint_help[] = {
@@ -61,6 +71,7 @@ static const struct endpoint_help saac_endpoint_help[] = {
 	{ "get-charge", MTA_CLI_SAAC "get-charge transactionId", 1 },
 	{ "list-payments", MTA_CLI_SAAC "list-payments from to", 2 },
 	{ "get-payment", MTA_CLI_SAAC "get-payment paymentId", 1 },
+	{ NULL, NULL, 0 }
 };
 
 static int print_sa_endpoints(void)
@@ -86,8 +97,7 @@ static int print_api_help(void)
 }
 
 static int check_args(int argc, const char *endpoint,
-		      const struct endpoint_help *eh, size_t nelem,
-		      int (*print_help)(void))
+		      const struct endpoint_help *eh, int (*print_help)(void))
 {
 	int ret = -1;
 	unsigned i = 0;
@@ -96,7 +106,7 @@ static int check_args(int argc, const char *endpoint,
 	if (!endpoint)
 		goto out_not_found;
 
-	for ( ; i < nelem; i++) {
+	for ( ; eh[i].ep != NULL; i++) {
 		if (strcmp(eh[i].ep, endpoint) == 0) {
 			if (eh[i].nargs == argc)
 				return 0;
@@ -124,86 +134,84 @@ static int get_end_of_period_statement(char *argv[], char **buf)
 	return mtd_sa_get_end_of_period_statement(argv[1], start, end, buf);
 }
 
-#define IS_EP(endpoint)		(strcmp(ep, endpoint) == 0)
-static int do_sa(int argc, char *argv[])
+#define IS_EP(endpoint)		(strcmp(argv[0], endpoint) == 0)
+static int do_sa(int argc, char *argv[], char **buf)
 {
-	const char *ep = argv[0];	/* endpoint */
-	char *buf = NULL;
-	json_t *rootbuf;
-	const struct endpoint_help *eh = sa_endpoint_help;
-	int nr = sizeof(sa_endpoint_help) / sizeof(sa_endpoint_help[0]);
 	int err;
 
-	err = check_args(argc - 1, ep, eh, nr, print_sa_endpoints);
+	err = check_args(argc, argv[0], sa_endpoint_help, print_sa_endpoints);
 	if (err)
 		return err;
 
 	if (IS_EP("list-employments"))
-		err = mtd_sa_list_employments(&buf);
+		return mtd_sa_list_employments(buf);
 	else if (IS_EP("get-employment"))
-		err = mtd_sa_get_employment(argv[1], &buf);
+		return mtd_sa_get_employment(argv[1], buf);
 	else if (IS_EP("list-obligations"))
-		err = mtd_sa_list_obligations(argv[1], &buf);
+		return mtd_sa_list_obligations(argv[1], buf);
 	else if (IS_EP("list-periods"))
-		err = mtd_sa_list_periods(argv[1], &buf);
+		return mtd_sa_list_periods(argv[1], buf);
 	else if (IS_EP("create-period"))
-		err = mtd_sa_create_period(argv[1], argv[2], &buf);
+		return mtd_sa_create_period(argv[1], argv[2], buf);
 	else if (IS_EP("get-period"))
-		err = mtd_sa_get_period(argv[1], argv[2], &buf);
+		return mtd_sa_get_period(argv[1], argv[2], buf);
 	else if (IS_EP("update-period"))
-		err = mtd_sa_update_period(argv[1], argv[2], argv[3], &buf);
+		return mtd_sa_update_period(argv[1], argv[2], argv[3], buf);
 	else if (IS_EP("get-annual-summary"))
-		err = mtd_sa_get_annual_summary(argv[1], argv[2], &buf);
+		return mtd_sa_get_annual_summary(argv[1], argv[2], buf);
 	else if (IS_EP("update-annual-summary"))
-		err = mtd_sa_update_annual_summary(argv[1], argv[2], argv[3],
-						   &buf);
+		return mtd_sa_update_annual_summary(argv[1], argv[2], argv[3],
+						    buf);
 	else if (IS_EP("submit-end-of-period-statement"))
-		err = mtd_sa_submit_end_of_period_statement(argv[1], argv[2],
-							    argv[3], argv[4],
-							    &buf);
+		return mtd_sa_submit_end_of_period_statement(argv[1], argv[2],
+							     argv[3], argv[4],
+							     buf);
 	else if (IS_EP("get-end-of-period-statement"))
-		err = get_end_of_period_statement(argv, &buf);
+		return get_end_of_period_statement(argv, buf);
 
-	if (!buf)
-		return err;
-
-	rootbuf = json_loads(buf, 0, NULL);
-	json_dumpf(rootbuf, stdout, JSON_INDENT(4));
-	printf("\n");
-	json_decref(rootbuf);
-
-	free(buf);
-
-	return err;
+	return -1;
 }
 
-static int do_saac(int argc, char *argv[])
+static int do_saac(int argc, char *argv[], char **buf)
 {
-	const char *ep = argv[0];	/* endpoint */
-	char *buf = NULL;
-	json_t *rootbuf;
-	const struct endpoint_help *eh = saac_endpoint_help;
-	int nr = sizeof(saac_endpoint_help) / sizeof(saac_endpoint_help[0]);
 	int err;
 
-	err = check_args(argc - 1, ep, eh, nr, print_saac_endpoints);
+	err = check_args(argc, argv[0], saac_endpoint_help,
+			 print_saac_endpoints);
 	if (err)
 		return err;
 
 	if (IS_EP("get-balance"))
-		err = mtd_saac_get_balance(&buf);
+		return mtd_saac_get_balance(buf);
 	else if (IS_EP("list-transactions"))
-		err = mtd_saac_list_transactions(argv[1], argv[2], &buf);
+		return mtd_saac_list_transactions(argv[1], argv[2], buf);
 	else if (IS_EP("get-transaction"))
-		err = mtd_saac_get_transaction(argv[1], &buf);
+		return mtd_saac_get_transaction(argv[1], buf);
 	else if (IS_EP("list-charges"))
-		err = mtd_saac_list_charges(argv[1], argv[2], &buf);
+		return mtd_saac_list_charges(argv[1], argv[2], buf);
 	else if (IS_EP("get-charge"))
-		err = mtd_saac_get_charge(argv[1], &buf);
+		return mtd_saac_get_charge(argv[1], buf);
 	else if (IS_EP("list-payments"))
-		err = mtd_saac_list_payments(argv[1], argv[2], &buf);
+		return mtd_saac_list_payments(argv[1], argv[2], buf);
 	else if (IS_EP("get-payment"))
-		err = mtd_saac_get_payment(argv[1], &buf);
+		return mtd_saac_get_payment(argv[1], buf);
+
+	return -1;
+}
+
+static int do_mtd_api(const char *ep, int argc, char *argv[])
+{
+	char *buf = NULL;
+	json_t *rootbuf;
+	int i = 0;
+	int err = -1;
+
+	for ( ; api_ep_map[i].api != NULL; i++) {
+		if (strcmp(api_ep_map[i].api, ep) != 0)
+			continue;
+
+		err = api_ep_map[i].api_func(argc, argv, &buf);
+	}
 
 	if (!buf)
 		return err;
@@ -248,10 +256,11 @@ static int do_init(void)
 	return err;
 }
 
-#define IS_API(api)		(strcmp(api, p) == 0)
+
+#define IS_API(api)		(strcmp(api, ep) == 0)
 static int dispatcher(int argc, char *argv[])
 {
-	const char *p = argv[0];
+	const char *ep = argv[0];
 	int err = -1;
 
 	if (IS_API("init"))
@@ -260,10 +269,8 @@ static int dispatcher(int argc, char *argv[])
 		err = do_oauth();
 	else if (IS_API("config"))
 		err = do_config();
-	else if (IS_API("sa"))
-		err = do_sa(argc - 1, argv + 1);
-	else if (IS_API("saac"))
-		err = do_saac(argc - 1, argv + 1);
+	else
+		err = do_mtd_api(ep, argc - 2, argv + 1);
 
 	return err;
 }

commit ac74fc7fcecc0cc191313ec483bb1c5cd8f10978

Split the endpoint APIs out into their own files
This splits the sa & saac endpoint APIs out into their own files. This
makes it even more clearer as to what needs to be done to add new
endpoints providing nice separation with the main mtd-cli.c file where
only two bits now need modifying when adding new endpoints, an entry
needs only be added to the APIS #define and the api_ep_map[] struct
array.

This also fixes the build which I inadvertently broke in commit 109a0c9
("mtd-cli: Do some refactoring").

Fixes: 109a0c9 ("mtd-cli: Do some refactoring")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-cli-sa.c
@@ -1,0 +1,102 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-sa.c - Make Tax Digital - Self-Assessment
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#include <stdio.h>
+
+#include <libmtdac/mtd-sa.h>
+
+#include "mtd-cli.h"
+
+#define MTD_CLI_SA	MTD_CLI " sa "
+
+#define SA_ENDPOINTS \
+"list-employments get-employment list-obligations list-periods create-period\n"\
+"get-period update-period get-annual-summary update-annual-summary\n"\
+"submit-end-of-period-statement get-end-of-period-statement"
+
+static const struct endpoint_help sa_endpoint_help[] = {
+	{ "list-employments", MTD_CLI_SA "list-employments", 0 },
+	{ "get-employment", MTD_CLI_SA "get-employment selfEmploymentId", 1 },
+	{ "list-obligations", MTD_CLI_SA "list-obligations selfEmploymentId",
+	  1 },
+	{ "list-periods", MTD_CLI_SA "list-periods selfEmploymentId", 1 },
+	{ "create-period", MTD_CLI_SA "create-period <file> selfEmploymentId",
+	  2 },
+	{ "get-period", MTD_CLI_SA "get-period selfEmploymentId periodId", 2 },
+	{ "update-period",
+	  MTD_CLI_SA "update-period <file> selfEmploymentId periodId", 3 },
+	{ "get-annual-summary",
+	  MTD_CLI_SA "get-annual-summary selfEmploymentId taxYear", 2 },
+	{ "update-annual-summary",
+	  MTD_CLI_SA "update-annual-summary <file> selfEmploymentId taxYear",
+	  3 },
+	{ "submit-end-of-period-statement",
+	  MTD_CLI_SA
+	   "submit-end-of-period-statement <file> selfEmploymentId start end",
+	  3 },
+	{ "get-end-of-period-statement",
+	  MTD_CLI_SA
+	   "get-end-of-period-statement selfEmploymentId start|- end|-",
+	  3 },
+	{ NULL, NULL, 0 }
+};
+
+static int print_sa_endpoints(void)
+{
+	printf("Available self-assessment endpoints :-\n\n%s\n", SA_ENDPOINTS);
+
+	return -1;
+}
+
+static int get_end_of_period_statement(char *argv[], char **buf)
+{
+	const char *start;
+	const char *end;
+
+	start = *argv[2] == '-' ? NULL : argv[2];
+	end = *argv[3] == '-' ? NULL : argv[3];
+
+	return mtd_sa_get_end_of_period_statement(argv[1], start, end, buf);
+}
+
+int do_sa(int argc, char *argv[], char **buf)
+{
+	int err;
+
+	err = check_args(argc, argv[0], sa_endpoint_help, print_sa_endpoints);
+	if (err)
+		return err;
+
+	if (IS_EP("list-employments"))
+		return mtd_sa_list_employments(buf);
+	else if (IS_EP("get-employment"))
+		return mtd_sa_get_employment(argv[1], buf);
+	else if (IS_EP("list-obligations"))
+		return mtd_sa_list_obligations(argv[1], buf);
+	else if (IS_EP("list-periods"))
+		return mtd_sa_list_periods(argv[1], buf);
+	else if (IS_EP("create-period"))
+		return mtd_sa_create_period(argv[1], argv[2], buf);
+	else if (IS_EP("get-period"))
+		return mtd_sa_get_period(argv[1], argv[2], buf);
+	else if (IS_EP("update-period"))
+		return mtd_sa_update_period(argv[1], argv[2], argv[3], buf);
+	else if (IS_EP("get-annual-summary"))
+		return mtd_sa_get_annual_summary(argv[1], argv[2], buf);
+	else if (IS_EP("update-annual-summary"))
+		return mtd_sa_update_annual_summary(argv[1], argv[2], argv[3],
+						    buf);
+	else if (IS_EP("submit-end-of-period-statement"))
+		return mtd_sa_submit_end_of_period_statement(argv[1], argv[2],
+							     argv[3], argv[4],
+							     buf);
+	else if (IS_EP("get-end-of-period-statement"))
+		return get_end_of_period_statement(argv, buf);
+
+	return -1;
+}

src/mtd-cli-sa.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-sa.h - Make Tax Digital CLI - Self-Assessment
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#ifndef _MTD_CLI_SA_H_
+#define _MTD_CLI_SA_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int do_sa(int argc, char *argv[], char **buf);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MTD_CLI_SA_H_ */

src/mtd-cli-saac.c
@@ -1,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-saac.c - make Tax Digital - Self-Assessment Accounts
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#include <stdio.h>
+
+#include <libmtdac/mtd-saac.h>
+
+#include "mtd-cli.h"
+
+#define MTD_CLI_SAAC	MTD_CLI " saac "
+
+#define SAAC_ENDPOINTS \
+"get-balance list-transactions get-transaction list-charges get-charge\n"\
+"list-payments get-payment"\
+
+static const struct endpoint_help saac_endpoint_help[] = {
+	{ "get-balance", MTD_CLI_SAAC "get-balance", 0 },
+	{ "list-transactions", MTD_CLI_SAAC "list-transactions from to", 2 },
+	{ "get-transaction", MTD_CLI_SAAC "get-transaction transactionId", 1 },
+	{ "list-charges", MTD_CLI_SAAC "list-charges from to", 2 },
+	{ "get-charge", MTD_CLI_SAAC "get-charge transactionId", 1 },
+	{ "list-payments", MTD_CLI_SAAC "list-payments from to", 2 },
+	{ "get-payment", MTD_CLI_SAAC "get-payment paymentId", 1 },
+	{ NULL, NULL, 0 }
+};
+
+static int print_saac_endpoints(void)
+{
+	printf("Available self-assessment accounts endpoints :-\n\n%s\n",
+	       SAAC_ENDPOINTS);
+
+	return -1;
+}
+
+int do_saac(int argc, char *argv[], char **buf)
+{
+	int err;
+
+	err = check_args(argc, argv[0], saac_endpoint_help,
+			 print_saac_endpoints);
+	if (err)
+		return err;
+
+	if (IS_EP("get-balance"))
+		return mtd_saac_get_balance(buf);
+	else if (IS_EP("list-transactions"))
+		return mtd_saac_list_transactions(argv[1], argv[2], buf);
+	else if (IS_EP("get-transaction"))
+		return mtd_saac_get_transaction(argv[1], buf);
+	else if (IS_EP("list-charges"))
+		return mtd_saac_list_charges(argv[1], argv[2], buf);
+	else if (IS_EP("get-charge"))
+		return mtd_saac_get_charge(argv[1], buf);
+	else if (IS_EP("list-payments"))
+		return mtd_saac_list_payments(argv[1], argv[2], buf);
+	else if (IS_EP("get-payment"))
+		return mtd_saac_get_payment(argv[1], buf);
+
+	return -1;
+}

src/mtd-cli-saac.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-saac.h - Make Tax Digital CLI - Self-Assessment Accounts
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#ifndef _MTD_CLI_SAAC_H_
+#define _MTD_CLI_SAAC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int do_saac(int argc, char *argv[], char **buf);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MTD_CLI_SAAC_H_ */

src/mtd-cli.c
@@ -1,3 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli.c - Make Tax Digital CLI
+ *
+ * Copyright (C) 2020           Andrew Clayton <andrew@digital-domain.net>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -9,19 +17,11 @@
 
 #include <jansson.h>
 
-#define MTD_CLI		"mtd-cli"
+#include "mtd-cli.h"
+#include "mtd-cli-sa.h"
+#include "mtd-cli-saac.h"
 
 #define APIS		"init oauth config sa saac"
-#define SA_ENDPOINTS \
-"list-employments get-employment list-obligations list-periods create-period\n"\
-"get-period update-period get-annual-summary update-annual-summary\n"\
-"submit-end-of-period-statement get-end-of-period-statement"
-#define SAAC_ENDPOINTS \
-"get-balance list-transactions get-transaction list-charges get-charge\n"\
-"list-payments get-payment"\
-
-#define MTD_CLI_SA	MTD_CLI " sa "
-#define MTA_CLI_SAAC	MTD_CLI " saac "
 
 static const struct api_ep {
 	const char *api;
@@ -32,63 +32,6 @@ static const struct api_ep {
 	{ NULL, NULL }
 };
 
-struct endpoint_help {
-	const char *ep;
-	const char *use;
-	const int nargs;
-};
-
-static const struct endpoint_help sa_endpoint_help[] = {
-	{ "list-employments", MTD_CLI_SA "list-employments", 0 },
-	{ "get-employment", MTD_CLI_SA "get-employment selfEmploymentId", 1 },
-	{ "list-obligations", MTD_CLI_SA "list-obligations selfEmploymentId",
-	   1 },
-	{ "list-periods", MTD_CLI_SA "list-periods selfEmploymentId", 1 },
-	{ "create-period", MTD_CLI_SA "create-period <file> selfEmploymentId",
-	   2 },
-	{ "get-period", MTD_CLI_SA "get-period selfEmploymentId periodId", 2 },
-	{ "update-period",
-	   MTD_CLI_SA "update-period <file> selfEmploymentId periodId", 3 },
-	{ "get-annual-summary",
-	   MTD_CLI_SA "get-annual-summary selfEmploymentId taxYear", 2 },
-	{ "update-annual-summary",
-	   MTD_CLI_SA "update-annual-summary <file> selfEmploymentId taxYear",
-	   3 },
-	{ "submit-end-of-period-statement",
-	   MTD_CLI_SA "submit-end-of-period-statement <file> selfEmploymentId start end",
-	   3 },
-	{ "get-end-of-period-statement",
-	   MTD_CLI_SA "get-end-of-period-statement selfEmploymentId start|- end|-",
-	   3 },
-	{ NULL, NULL, 0 }
-};
-
-static const struct endpoint_help saac_endpoint_help[] = {
-	{ "get-balance", MTA_CLI_SAAC "get-balance", 0 },
-	{ "list-transactions", MTA_CLI_SAAC "list-transactions from to", 2 },
-	{ "get-transaction", MTA_CLI_SAAC "get-transaction transactionId", 1 },
-	{ "list-charges", MTA_CLI_SAAC "list-charges from to", 2 },
-	{ "get-charge", MTA_CLI_SAAC "get-charge transactionId", 1 },
-	{ "list-payments", MTA_CLI_SAAC "list-payments from to", 2 },
-	{ "get-payment", MTA_CLI_SAAC "get-payment paymentId", 1 },
-	{ NULL, NULL, 0 }
-};
-
-static int print_sa_endpoints(void)
-{
-	printf("Available self-assessment endpoints :-\n\n%s\n", SA_ENDPOINTS);
-
-	return -1;
-}
-
-static int print_saac_endpoints(void)
-{
-	printf("Available self-assessment accounts endpoints :-\n\n%s\n",
-	       SAAC_ENDPOINTS);
-
-	return -1;
-}
-
 static int print_api_help(void)
 {
 	printf("Available APIs :-\n\n%s\n", APIS);
@@ -96,8 +39,8 @@ static int print_api_help(void)
 	return -1;
 }
 
-static int check_args(int argc, const char *endpoint,
-		      const struct endpoint_help *eh, int (*print_help)(void))
+int check_args(int argc, const char *endpoint, const struct endpoint_help *eh,
+	       int (*print_help)(void))
 {
 	int ret = -1;
 	unsigned i = 0;
@@ -121,82 +64,6 @@ out_not_found:
 		print_help();
 
 	return ret;
-}
-
-static int get_end_of_period_statement(char *argv[], char **buf)
-{
-	const char *start;
-	const char *end;
-
-	start = *argv[2] == '-' ? NULL : argv[2];
-	end = *argv[3] == '-' ? NULL : argv[3];
-
-	return mtd_sa_get_end_of_period_statement(argv[1], start, end, buf);
-}
-
-#define IS_EP(endpoint)		(strcmp(argv[0], endpoint) == 0)
-static int do_sa(int argc, char *argv[], char **buf)
-{
-	int err;
-
-	err = check_args(argc, argv[0], sa_endpoint_help, print_sa_endpoints);
-	if (err)
-		return err;
-
-	if (IS_EP("list-employments"))
-		return mtd_sa_list_employments(buf);
-	else if (IS_EP("get-employment"))
-		return mtd_sa_get_employment(argv[1], buf);
-	else if (IS_EP("list-obligations"))
-		return mtd_sa_list_obligations(argv[1], buf);
-	else if (IS_EP("list-periods"))
-		return mtd_sa_list_periods(argv[1], buf);
-	else if (IS_EP("create-period"))
-		return mtd_sa_create_period(argv[1], argv[2], buf);
-	else if (IS_EP("get-period"))
-		return mtd_sa_get_period(argv[1], argv[2], buf);
-	else if (IS_EP("update-period"))
-		return mtd_sa_update_period(argv[1], argv[2], argv[3], buf);
-	else if (IS_EP("get-annual-summary"))
-		return mtd_sa_get_annual_summary(argv[1], argv[2], buf);
-	else if (IS_EP("update-annual-summary"))
-		return mtd_sa_update_annual_summary(argv[1], argv[2], argv[3],
-						    buf);
-	else if (IS_EP("submit-end-of-period-statement"))
-		return mtd_sa_submit_end_of_period_statement(argv[1], argv[2],
-							     argv[3], argv[4],
-							     buf);
-	else if (IS_EP("get-end-of-period-statement"))
-		return get_end_of_period_statement(argv, buf);
-
-	return -1;
-}
-
-static int do_saac(int argc, char *argv[], char **buf)
-{
-	int err;
-
-	err = check_args(argc, argv[0], saac_endpoint_help,
-			 print_saac_endpoints);
-	if (err)
-		return err;
-
-	if (IS_EP("get-balance"))
-		return mtd_saac_get_balance(buf);
-	else if (IS_EP("list-transactions"))
-		return mtd_saac_list_transactions(argv[1], argv[2], buf);
-	else if (IS_EP("get-transaction"))
-		return mtd_saac_get_transaction(argv[1], buf);
-	else if (IS_EP("list-charges"))
-		return mtd_saac_list_charges(argv[1], argv[2], buf);
-	else if (IS_EP("get-charge"))
-		return mtd_saac_get_charge(argv[1], buf);
-	else if (IS_EP("list-payments"))
-		return mtd_saac_list_payments(argv[1], argv[2], buf);
-	else if (IS_EP("get-payment"))
-		return mtd_saac_get_payment(argv[1], buf);
-
-	return -1;
 }
 
 static int do_mtd_api(const char *ep, int argc, char *argv[])

src/mtd-cli.h
@@ -1,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli.h - Make Tax Digital CLI
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#ifndef _MTD_CLI_H_
+#define _MTD_CLI_H_
+
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MTD_CLI			"mtd-cli"
+
+#define IS_EP(endpoint)		(strcmp(argv[0], endpoint) == 0)
+
+struct endpoint_help {
+	const char *ep;
+	const char *use;
+	const int nargs;
+};
+
+extern int check_args(int argc, const char *endpoint,
+		      const struct endpoint_help *eh, int (*print_help)(void));
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MTD_CLI_H_ */