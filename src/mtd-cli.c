ac000/mtd-cli

commit 7ff57caf5c54ba9d86cd340813475ff6ba1990cc

mtd-cli: Add support for the 'Test Fraud Prevention Headers' API
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-cli.c
@@ -22,8 +22,9 @@
 #include "mtd-cli-ic.h"
 #include "mtd-cli-ni.h"
 #include "mtd-cli-test-cu.h"
+#include "mtd-cli-test-fph.h"
 
-#define APIS		"init oauth config sa saac ic ni test-cu"
+#define APIS		"init oauth config sa saac ic ni test-cu test-fph"
 
 static const struct api_ep {
 	const char *api;
@@ -48,6 +49,10 @@ static const struct api_ep {
 	{
 		.api = "test-cu",
 		.endpoint = &test_cu_endpoint
+	},
+	{
+		.api = "test-fph",
+		.endpoint = &test_fph_endpoint
 	},
 	{ NULL, NULL }
 };
@@ -211,7 +216,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	err = mtd_init(MTD_OPT_LOG_ERR);
+	err = mtd_init(MTD_OPT_LOG_ERR|MTD_OPT_SND_ANTI_FRAUD_HDRS);
 	if (err && strcmp(argv[1], "init") != 0) {
 		if (err == MTD_ERR_MISSING_CONFIG)
 			fprintf(stderr, "Please run 'mtd-cli init\n");

src/mtd-cli-test-fph.c
@@ -1,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-test-fph.c - Make Tax Digital - Test Fraud Prevention Headers
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#include <stdio.h>
+
+#include <libmtdac/mtd-test-fph.h>
+
+#include "mtd-cli.h"
+
+#define MTD_CLI_CMD	MTD_CLI " test-fph "
+#define API_NAME	"Test Fraud Prevention Headers"
+
+#define ENDPOINTS	"validate"
+
+static int print_endpoints(void)
+{
+	printf("Available " API_NAME " endpoints :-\n\n%s\n", ENDPOINTS);
+
+	return -1;
+}
+
+static const struct endpoint endpoints[] = {
+	{
+		.name = "validate",
+		.api_func = {
+			.func_0 = &mtd_test_fph_validate
+		},
+		.nargs = 0,
+		.use = MTD_CLI_CMD "validate"
+	},
+	{ NULL, { NULL }, 0, NULL}
+};
+
+const struct _endpoint test_fph_endpoint = {
+	.endpoints = endpoints,
+	.print_help = &print_endpoints
+};

src/mtd-cli-test-fph.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-test-fph.h - Make Tax Digital CLI - Test Fraud Prevention Headers
+ *
+ * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#ifndef _MTD_CLI_TEST_FPH_H_
+#define _MTD_CLI_TEST_FPH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct _endpoint test_fph_endpoint;
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MTD_CLI_TEST_FPH_H_ */

commit aed425b82e53fb2292dd0d916ee948d8ba331fac

mtd-cli: Undo MTD_OPT_SND_ANTI_FRAUD_HDRS
Commit 7ff57ca ("mtd-cli: Add support for the 'Test Fraud Prevention
Headers' API") inadvertently enabled the sending of the Fraud Prevention
Headers.

I'll want to come up with a better way of enabling/disabling this
feature for the short term at least...

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-cli.c
@@ -216,7 +216,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	err = mtd_init(MTD_OPT_LOG_ERR|MTD_OPT_SND_ANTI_FRAUD_HDRS);
+	err = mtd_init(MTD_OPT_LOG_ERR);
 	if (err && strcmp(argv[1], "init") != 0) {
 		if (err == MTD_ERR_MISSING_CONFIG)
 			fprintf(stderr, "Please run 'mtd-cli init\n");