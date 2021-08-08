73c355f066fec9289b701f8ead4d2fe3a1e91310
Andrew Clayton <andrew@digital-domain.net>
2021-08-08 19:21:26 +0100

    mtd-cli-bsas.c: Small cleanup of endpoints[]
    Put the closing '}' and opening '{' on the same line to slightly reduce
    number of mostly empty lines.
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- src/mtd-cli-bsas.c
+++ src/mtd-cli-bsas.c
@@ -33,8 +33,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_1,
 		.nr_req_args = 0,
 		.use = "bsas-list-summaries [[selfEmploymentId=][,[typeOfBusiness={self-employment,uk-property-non-fhl,uk-property-fhl}][,[taxYear=YYYY-YY]]]]"
-	},
-	{
+	}, {
 		.name = "bsas-trigger-summary",
 		.api_func = {
 			.func_1d = mtd_bsas_trigger_summary
@@ -42,8 +41,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_1d,
 		.nr_req_args = 1,
 		.use = "bsas-trigger-summary <file>"
-	},
-	{
+	}, {
 		.name = "bsas-se-get-summary",
 		.api_func = {
 			.func_2 = mtd_bsas_se_get_summary
@@ -51,8 +49,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_2,
 		.nr_req_args = 1,
 		.use = "bsas-se-get-summary bsasId [adjustedStatus={true,false}]"
-	},
-	{
+	}, {
 		.name = "bsas-se-list-summary-adjustments",
 		.api_func = {
 			.func_1 = mtd_bsas_se_list_summary_adjustments
@@ -60,8 +57,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_1,
 		.nr_req_args = 1,
 		.use = "bsas-se-list-summary-adjustments bsasId"
-	},
-	{
+	}, {
 		.name = "bsas-se-update-summary-adjustments",
 		.api_func = {
 			.func_2d = mtd_bsas_se_update_summary_adjustments
@@ -69,8 +65,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_2d,
 		.nr_req_args = 2,
 		.use = "bsas-se-update-summary-adjustments <file> bsasId"
-	},
-	{
+	}, {
 		.name = "bsas-pb-get-summary",
 		.api_func = {
 			.func_2 = mtd_bsas_pb_get_summary
@@ -78,8 +73,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_2,
 		.nr_req_args = 1,
 		.use = "bsas-pb-get-summary bsasId [adjustedStatus={true,false}]"
-	},
-	{
+	}, {
 		.name = "bsas-pb-list-summary-adjustments",
 		.api_func = {
 			.func_1 = mtd_bsas_pb_list_summary_adjustments
@@ -87,8 +81,7 @@ static const struct endpoint endpoints[] = {
 		.func = FUNC_1,
 		.nr_req_args = 1,
 		.use = "bsas-pb-list-summary-adjustments bsasId"
-	},
-	{
+	}, {
 		.name = "bsas-pb-update-summary-adjustments",
 		.api_func = {
 			.func_2d = mtd_bsas_pb_update_summary_adjustments
