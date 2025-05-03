ac000/mtd-cli

commit 8c31f577ba8ca183c89f5cf6ee3e0121dca85dfa

Big update for *new* libmtdac
libmtdac has undergone a major revamp, this aligns to it.

Various endpoint commands have been removed (as they have in libmtdac).
It's likely some (and others) will be added back as and when...

Most of the endpoint commands documentation has been removed as it's
outdated and it really just duplicates the official documentation. Also
the commands somewhat document what they expect. E.g.

  $ ./mtd-cli API endpoint ?

will show what arguments that command expects.

Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/mtd-cli.c
@@ -29,53 +29,22 @@
 
 #define MAX_ARGV	7
 
+#define EP_MAP_ENT(ep)	{ .api = #ep, .endpoint = &ep##_endpoint }
+
 static const struct api_ep {
 	const char *api;
 	const struct _endpoint *endpoint;
 } api_ep_map[] = {
-	{
-		.api = "bd",
-		.endpoint = &bd_endpoint
-	}, {
-		.api = "biss",
-		.endpoint = &biss_endpoint
-	}, {
-		.api = "bsas",
-		.endpoint = &bsas_endpoint
-	}, {
-		.api = "ibeops",
-		.endpoint = &ibeops_endpoint
-	}, {
-		.api = "ic",
-		.endpoint = &ic_endpoint
-	}, {
-		.api = "id",
-		.endpoint = &id_endpoint
-	}, {
-		.api = "il",
-		.endpoint = &il_endpoint
-	}, {
-		.api = "isi",
-		.endpoint = &isi_endpoint
-	}, {
-		.api = "ob",
-		.endpoint = &ob_endpoint
-	}, {
-		.api = "sa",
-		.endpoint = &sa_endpoint
-	}, {
-		.api = "saac",
-		.endpoint = &saac_endpoint
-	}, {
-		.api = "vat",
-		.endpoint = &vat_endpoint
-	}, {
-		.api = "test-cu",
-		.endpoint = &test_cu_endpoint
-	}, {
-		.api = "test-fph",
-		.endpoint = &test_fph_endpoint
-	},
+	EP_MAP_ENT(bd),
+	EP_MAP_ENT(ical),
+	EP_MAP_ENT(ilos),
+	EP_MAP_ENT(isi),
+	EP_MAP_ENT(ob),
+	EP_MAP_ENT(pb),
+	EP_MAP_ENT(seb),
+
+	EP_MAP_ENT(test_cu),
+	EP_MAP_ENT(test_fph),
 
 	{ }
 };
@@ -151,7 +120,7 @@ static int do_api_func(const struct endpoint *ep, int argc, char *argv[],
 		       char **buf)
 {
 	int i;
-	const char *args[MAX_ARGV] = { NULL };
+	const char *args[MAX_ARGV] = {};
 	const struct endpoint *eps = ep;
 	char *ptr;
 	char qs[129];
@@ -174,36 +143,13 @@ static int do_api_func(const struct endpoint *ep, int argc, char *argv[],
 		if (strcmp(eps->name, argv[0]) != 0)
 			continue;
 
-		switch (eps->func) {
-		case FUNC_1d:
-		case FUNC_2d:
-		case FUNC_3d:
-		case FUNC_4d:
+		if (eps->file_data) {
 			dsctx.data_src.file = args[0];
 			dsctx.src_type = MTD_DATA_SRC_FILE;
-		default:
-			break;
 		}
 
-		switch (eps->func) {
-		case FUNC_0:
-			return eps->func_0(buf);
-		case FUNC_1:
-			return eps->func_1(args[0], buf);
-		case FUNC_1d:
-			return eps->func_1d(&dsctx, buf);
-		case FUNC_2:
-			return eps->func_2(args[0], args[1], buf);
-		case FUNC_2d:
-			return eps->func_2d(&dsctx, args[1], buf);
-		case FUNC_3:
-			return eps->func_3(args[0], args[1], args[2], buf);
-		case FUNC_3d:
-			return eps->func_3d(&dsctx, args[1], args[2], buf);
-		case FUNC_4d:
-			return eps->func_4d(&dsctx, args[1], args[2], args[3],
-					    buf);
-		}
+		return mtd_ep(eps->api_ep, eps->file_data ? &dsctx : NULL, buf,
+			      args + eps->file_data);
 	}
 
 	/* I don't think we can ever reach here... */
@@ -243,12 +189,12 @@ static int init_creds(void)
 {
 	int err;
 
-	err = mtd_init_creds(MTD_EP_API_ITSA);
+	err = mtd_init_creds(MTD_API_SCOPE_ITSA);
 	if (err)
 		return err;
 
 	printf("\n");
-	err = mtd_init_creds(MTD_EP_API_VAT|MTD_EP_API_ADD);
+	err = mtd_init_creds(MTD_API_SCOPE_VAT|MTD_API_SCOPE_ADD);
 	if (err)
 		return err;
 
@@ -261,12 +207,12 @@ static int init_auth(void)
 {
 	int err;
 
-	err = mtd_init_auth(MTD_EP_API_ITSA, ITSA_SCOPES);
+	err = mtd_init_auth(MTD_API_SCOPE_ITSA, ITSA_SCOPES);
 	if (err)
 		return err;
 
 	printf("\n");
-	err = mtd_init_auth(MTD_EP_API_VAT|MTD_EP_API_ADD, VAT_SCOPES);
+	err = mtd_init_auth(MTD_API_SCOPE_VAT|MTD_API_SCOPE_ADD, VAT_SCOPES);
 	if (err)
 		return err;
 

src/endpoints.h
@@ -10,17 +10,13 @@
 #define _ENDPOINTS_H_
 
 extern const struct _endpoint bd_endpoint;
-extern const struct _endpoint biss_endpoint;
 extern const struct _endpoint bsas_endpoint;
-extern const struct _endpoint ibeops_endpoint;
-extern const struct _endpoint ic_endpoint;
-extern const struct _endpoint il_endpoint;
-extern const struct _endpoint id_endpoint;
+extern const struct _endpoint ical_endpoint;
+extern const struct _endpoint ilos_endpoint;
 extern const struct _endpoint isi_endpoint;
 extern const struct _endpoint ob_endpoint;
-extern const struct _endpoint saac_endpoint;
-extern const struct _endpoint sa_endpoint;
-extern const struct _endpoint vat_endpoint;
+extern const struct _endpoint pb_endpoint;
+extern const struct _endpoint seb_endpoint;
 
 extern const struct _endpoint test_cu_endpoint;
 extern const struct _endpoint test_fph_endpoint;

src/mtd-cli-bd.c
@@ -3,31 +3,37 @@
 /*
  * mtd-cli-bd.c - Make Tax Digital - Business Details
  *
- * Copyright (C) 2021 - 2022	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2021 - 2025	Andrew Clayton <ac@sigsegv.uk>
  */
 
-#include <libmtdac/mtd-bd.h>
+#include <stdbool.h>
+
+#include <libmtdac/mtd.h>
 
 #include "mtd-cli.h"
 
 #define API	bd
 
 #define API_NAME "Business Details"
-#define CMDS "list get"
+#define CMDS "list get amend-qpt"
 
 static const struct endpoint endpoints[] = {
 	{
-		.name = "list",
-		.func_0 = mtd_bd_list,
-		.func = FUNC_0,
-		.nr_req_args = 0,
-		.args = ""
+		.name		= "list",
+		.api_ep		= MTD_API_EP_BD_LIST,
+		.nr_req_args	= 0,
+		.args		= ""
 	}, {
-		.name = "get",
-		.func_1 = mtd_bd_get,
-		.func = FUNC_1,
-		.nr_req_args = 1,
-		.args = "businessId"
+		.name		= "get",
+		.api_ep		= MTD_API_EP_BD_GET,
+		.nr_req_args	= 1,
+		.args		= "businessId"
+	}, {
+		.name		= "amend-qpt",
+		.api_ep		= MTD_API_EP_BD_AMEND_QPT,
+		.nr_req_args	= 3,
+		.file_data	= true,
+		.args		= "<file> businessId taxYear"
 	},
 
 	{ }

src/mtd-cli-biss.c
@@ -1,34 +1,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-/*
- * mtd-cli-biss.c - Make Tax Digital - Business Income Source Summary
- *
- * Copyright (C) 2020 - 2022	Andrew Clayton <andrew@digital-domain.net>
- */
-
-#include <libmtdac/mtd-biss.h>
-
-#include "mtd-cli.h"
-
-#define API	biss
-
-#define API_NAME "Business Income Source Summary"
-#define CMDS "get-summary"
-
-static const struct endpoint endpoints[] = {
-	{
-		.name = "get-summary",
-		.func_3 = mtd_biss_get_summary,
-		.func = FUNC_3,
-		.nr_req_args = 3,
-		.args = "typeOfBusiness taxYear businessId"
-	},
-
-	{ }
-};
-
-const struct _endpoint ENDPOINT = {
-	.endpoints = endpoints,
-	.api_name = API_NAME,
-	.cmds = CMDS
-};

src/mtd-cli-bsas.c
@@ -3,10 +3,12 @@
 /*
  * mtd-cli-bsas.c - Make Tax Digital - Business Source Adjustable Summary
  *
- * Copyright (C) 2020 - 2022	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2020 - 2025	Andrew Clayton <ac@sigsegv.uk>
  */
 
-#include <libmtdac/mtd-bsas.h>
+#include <stdbool.h>
+
+#include <libmtdac/mtd.h>
 
 #include "mtd-cli.h"
 
@@ -14,7 +16,7 @@
 
 #define API_NAME "Business Source Adjustable Summary"
 #define CMDS \
-"list-summaries trigger-summary\n\n"\
+"list trigger\n\n"\
 "Self-Employment\n\n"\
 "se-get-summary se-update-summary-adjustments\n\n"\
 "UK Property Business\n\n"\
@@ -24,53 +26,48 @@
 
 static const struct endpoint endpoints[] = {
 	{
-		.name = "list-summaries",
-		.func_1 = mtd_bsas_list_summaries,
-		.func = FUNC_1,
-		.nr_req_args = 0,
-		.args = "[[selfEmploymentId=][,[typeOfBusiness={self-employment,uk-property-non-fhl,uk-property-fhl,foreign-property-fhl-eea,foreign-property}][,[taxYear=YYYY-YY]]]]"
+		.name		= "list",
+		.api_ep		= MTD_API_EP_BSAS_LIST,
+		.nr_req_args	= 1,
+		.args		= "taxYear [typeOfBusiness={self-employment,uk-property-non-fhl,uk-property-fhl,foreign-property-fhl-eea,foreign-property}]"
 	}, {
-		.name = "trigger-summary",
-		.func_1d = mtd_bsas_trigger_summary,
-		.func = FUNC_1d,
-		.nr_req_args = 1,
-		.args = "<file>"
+		.name		= "trigger",
+		.api_ep		= MTD_API_EP_BSAS_TRIGGER,
+		.nr_req_args	= 1,
+		.file_data	= true,
+		.args		= "<file>"
 	}, {
-		.name = "se-get-summary",
-		.func_1 = mtd_bsas_se_get_summary,
-		.func = FUNC_1,
-		.nr_req_args = 1,
-		.args = "calculationId"
+		.name		= "se-get-summary",
+		.api_ep		= MTD_API_EP_BSAS_SE_GET,
+		.nr_req_args	= 2,
+		.args		= "calculationId taxYear"
 	}, {
-		.name = "se-update-summary-adjustments",
-		.func_2d = mtd_bsas_se_update_summary_adjustments,
-		.func = FUNC_2d,
-		.nr_req_args = 2,
-		.args = "<file> calculationId"
+		.name		= "se-update-summary-adjustments",
+		.api_ep		= MTD_API_EP_BSAS_SE_SUBMIT,
+		.nr_req_args	= 3,
+		.file_data	= true,
+		.args		= "<file> calculationId taxYear"
 	}, {
-		.name = "pb-get-summary",
-		.func_1 = mtd_bsas_pb_get_summary,
-		.func = FUNC_1,
-		.nr_req_args = 1,
-		.args = "calculationId"
+		.name		= "pb-get-summary",
+		.api_ep		= MTD_API_EP_BSAS_PB_GET,
+		.nr_req_args	= 2,
+		.args		= "calculationId taxYear"
 	}, {
-		.name = "pb-update-summary-adjustments",
-		.func_2d = mtd_bsas_pb_update_summary_adjustments,
-		.func = FUNC_2d,
-		.nr_req_args = 2,
-		.args = "<file> calculationId"
+		.name		= "pb-update-summary-adjustments",
+		.api_ep		= MTD_API_EP_BSAS_PB_SUBMIT,
+		.nr_req_args	= 3,
+		.args		= "<file> calculationId taxYear"
 	}, {
-		.name = "fp-get-summary",
-		.func_1 = mtd_bsas_fp_get_summary,
-		.func = FUNC_1,
-		.nr_req_args = 1,
-		.args = "calculationId"
+		.name		= "fp-get-summary",
+		.api_ep		= MTD_API_EP_BSAS_FP_GET,
+		.nr_req_args	= 2,
+		.args		= "calculationId taxYear"
 	}, {
-		.name = "fp-update-summary-adjustments",
-		.func_2d = mtd_bsas_fp_update_summary_adjustments,
-		.func = FUNC_2d,
-		.nr_req_args = 2,
-		.args = "<file> calculationId"
+		.name		= "fp-update-summary-adjustments",
+		.api_ep		= MTD_API_EP_BSAS_FP_SUBMIT,
+		.nr_req_args	= 3,
+		.file_data	= true,
+		.args		= "<file> calculationId taxYear"
 	},
 
 	{ }

src/mtd-cli-ibeops.c
@@ -1,34 +1,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-/*
- * mtd-cli-ibeops.c - Make Tax Digital - Individuals Business End of Period Statement
- *
- * Copyright (C) 2021 - 2022	Andrew Clayton <andrew@digital-domain.net>
- */
-
-#include <libmtdac/mtd-ibeops.h>
-
-#include "mtd-cli.h"
-
-#define API	ibeops
-
-#define API_NAME "Individuals Business End of Period Statement"
-#define CMDS "submit-end-of-period-statement"
-
-static const struct endpoint endpoints[] = {
-	{
-		.name = "submit-end-of-period-statement",
-		.func_1d = mtd_ibeops_submit_eops,
-		.func = FUNC_1d,
-		.nr_req_args = 1,
-		.args = "<file>"
-	},
-
-	{ }
-};
-
-const struct _endpoint ENDPOINT = {
-	.endpoints = endpoints,
-	.api_name = API_NAME,
-	.cmds = CMDS
-};

src/mtd-cli-ic.c
@@ -1,58 +1,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-/*
- * mtd-cli-ic.c - Make Tax Digital - Individual Calculations
- *
- * Copyright (C) 2020 - 2022	Andrew Clayton <andrew@digital-domain.net>
- */
-
-#include <stdbool.h>
-
-#include <libmtdac/mtd-ic.h>
-
-#include "mtd-cli.h"
-
-#define API	ic
-
-#define API_NAME "Individual Calculations"
-#define CMDS \
-"TAX Calclations\n\n"\
-"list-calculations trigger-calculation get-calculation\n\n"\
-"Final Declaration\n\n"\
-"final-declaration"
-
-static const struct endpoint endpoints[] = {
-	{
-		.name = "list-calculations",
-		.func_1 = mtd_ic_list_calculations,
-		.func = FUNC_1,
-		.nr_req_args = 0,
-		.args = "[taxYear=YYYY-YY]"
-	}, {
-		.name = "trigger-calculation",
-		.func_2 = mtd_ic_trigger_calculation,
-		.func = FUNC_2,
-		.nr_req_args = 1,
-		.args = "taxYear [finalDeclaration={true,false}]"
-	}, {
-		.name = "get-calculation",
-		.func_2 = mtd_ic_get_calculation,
-		.func = FUNC_2,
-		.nr_req_args = 2,
-		.args = "taxYear calculationId"
-	}, {
-		.name = "final-declaration",
-		.func_2 = mtd_ic_final_decl,
-		.func = FUNC_2,
-		.nr_req_args = 2,
-		.args = "taxYear calculationId"
-	},
-
-	{ }
-};
-
-const struct _endpoint ENDPOINT = {
-	.endpoints = endpoints,
-	.api_name = API_NAME,
-	.cmds = CMDS
-};

src/mtd-cli-ical.c
@@ -1,0 +1,58 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * mtd-cli-ical.c - Make Tax Digital - Individual Calculations
+ *
+ * Copyright (C) 2020 - 2025	Andrew Clayton <ac@sigsegv.uk>
+ */
+
+#include <libmtdac/mtd.h>
+
+#include "mtd-cli.h"
+
+#define API	ical
+
+#define API_NAME "Individual Calculations"
+#define CMDS \
+"TAX Calculations\n\n"\
+"list-calculations-old list-calculations trigger-calculation\n"\
+"get-calculation\n\n"\
+"Final Declaration\n\n"\
+"final-declaration"
+
+static const struct endpoint endpoints[] = {
+	{
+		.name		= "list-calculations-old",
+		.api_ep		= MTD_API_EP_ICAL_LIST_OLD,
+		.nr_req_args	= 1,
+		.args		= "taxYear=YYYY-YY"
+	}, {
+		.name		= "list-calculations",
+		.api_ep		= MTD_API_EP_ICAL_LIST,
+		.nr_req_args	= 1,
+		.args		= "taxYear [calculationType=in-year|intent-to-finalise|intent-to-amend|final-declaration|confirm-amendment]",
+	}, {
+		.name		= "trigger-calculation",
+		.api_ep		= MTD_API_EP_ICAL_TRIGGER,
+		.nr_req_args	= 2,
+		.args		= "taxYear calculationType"
+	}, {
+		.name		= "get-calculation",
+		.api_ep		= MTD_API_EP_ICAL_GET,
+		.nr_req_args	= 2,
+		.args		= "taxYear calculationId"
+	}, {
+		.name		= "final-declaration",
+		.api_ep		= MTD_API_EP_ICAL_FINAL_DECLARATION,
+		.nr_req_args	= 3,
+		.args		= "taxYear calculationId calculationType"
+	},
+
+	{ }
+};
+
+const struct _endpoint ENDPOINT = {
+	.endpoints = endpoints,
+	.api_name = API_NAME,
+	.cmds = CMDS
+};

commit 48249626591b874fda1d7322829ea12ac78bb99a

Actually enable the BSAS API
Fixes: 8c31f57 ("Big update for *new* libmtdac")
Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/mtd-cli.c
@@ -37,6 +37,7 @@ static const struct api_ep {
 } api_ep_map[] = {
 	EP_MAP_ENT(bd),
 	EP_MAP_ENT(biss),
+	EP_MAP_ENT(bsas),
 	EP_MAP_ENT(ical),
 	EP_MAP_ENT(ilos),
 	EP_MAP_ENT(isi),