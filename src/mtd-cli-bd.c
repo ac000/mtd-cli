450c17b5bc40dbc83cd806d260e04c9c016f77c2
Andrew Clayton <ac@sigsegv.uk>
2025-09-16 15:13:09 +0100

    Update to version 2.0 of 'Business Details' API
    Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

--- src/mtd-cli-bd.c
+++ src/mtd-cli-bd.c
@@ -15,9 +15,18 @@
 #define API	bd
 
 #define API_NAME "Business Details"
-#define CMDS "list get amend-qpt"
+#define CMDS \
+"Support\n\n"\
+"list get amend-qpt\n\n"\
+"Accounting Type\n\n"\
+"at-get at-amend\n\n"\
+"Periods of Account\n\n"\
+"poa-get poa-amend\n\n"\
+"Late Accounting Date Rule\n\n"\
+"ladr-get ladr-disapply ladr-withdraw"
 
 static const struct endpoint endpoints[] = {
+	/* Support */
 	{
 		.name		= "list",
 		.api_ep		= MTD_API_EP_BD_LIST,
@@ -34,6 +43,49 @@ static const struct endpoint endpoints[] = {
 		.nr_req_args	= 3,
 		.file_data	= true,
 		.args		= "<file> businessId taxYear"
+	},
+	/* Accounting Type */
+	{
+		.name		= "at-get",
+		.api_ep		= MTD_API_EP_BD_AT_GET,
+		.nr_req_args	= 2,
+		.args		= "businessId taxYear"
+	}, {
+		.name		= "at-amend",
+		.api_ep		= MTD_API_EP_BD_AT_AMEND,
+		.nr_req_args	= 3,
+		.file_data	= true,
+		.args		= "<file> businessId taxYear"
+	},
+	/* Periods of Account */
+	{
+		.name		= "poa-get",
+		.api_ep		= MTD_API_EP_BD_POA_GET,
+		.nr_req_args	= 2,
+		.args		= "businessId taxYear"
+	}, {
+		.name		= "poa-amend",
+		.api_ep		= MTD_API_EP_BD_POA_AMEND,
+		.nr_req_args	= 3,
+		.file_data	= true,
+		.args		= "<file> businessId taxYear"
+	},
+	/* Late Accounting Date Rule */
+	{
+		.name		= "ladr-get",
+		.api_ep		= MTD_API_EP_BD_LADR_GET,
+		.nr_req_args	= 2,
+		.args		= "businessId taxYear"
+	}, {
+		.name		= "ladr-disapply",
+		.api_ep		= MTD_API_EP_BD_LADR_DISAPPLY,
+		.nr_req_args	= 2,
+		.args		= "businessId taxYear"
+	}, {
+		.name		= "ladr-withdraw",
+		.api_ep		= MTD_API_EP_BD_LADR_WITHDRAW,
+		.nr_req_args	= 2,
+		.args		= "businessId taxYear"
 	},
 
 	{ }
